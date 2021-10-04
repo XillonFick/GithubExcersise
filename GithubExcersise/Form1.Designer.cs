b0ff4ff4a7f7b386533d5dba74159245dc8e1069
Xillon <60034172+Xillon@users.noreply.github.com>
2021-10-04 13:34:45 +0200

    Changed label

--- GithubExcersise/Form1.Designer.cs
+++ GithubExcersise/Form1.Designer.cs
@@ -37,9 +37,9 @@ namespace GithubExcersise
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(311, 117);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Hello";
             // 
             // Form1
             // 
